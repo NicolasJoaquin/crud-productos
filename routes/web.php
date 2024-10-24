f40c2b817c7723306d6bcc1309cdaaeb4282403f
Nicolas Diorio <nicolas.joaquin.diorio@gmail.com>
2024-10-23 21:02:19 -0300

    web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
+use App\Http\Middleware\AuthMiddleware;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(AuthMiddleware::class)->group(function () {
+    Route::get('/products', [ProductController::class, 'getProducts']);
+    Route::get('/products/{id}', [ProductController::class, 'getProduct']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'delete']);
 });
+
+Route::get('/csrf-token', function () {
+    return response()->json(['csrf_token' => csrf_token()]);
+});
+
+Route::post('/login', [AuthController::class, 'login']);
